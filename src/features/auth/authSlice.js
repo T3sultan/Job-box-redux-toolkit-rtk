6508d95d1d02dda8455ca303e0f83bfb4e79f502
unknown <{T3sultan}@users.noreply.github.com>
2023-01-29 12:54:43 +0600

    login and google login

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,4 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import auth from "../../firebase/firebase.config";
 
 const initialState = {
   email: "",
@@ -8,11 +15,93 @@ const initialState = {
   error: "",
 };
 
+export const createUser = createAsyncThunk(
+  "auth/createUser",
+  async ({ email, password }) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async ({ email, password }) => {
+    const data = await signInWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+export const googleLogin = createAsyncThunk("auth/googleLogin", async () => {
+  const googleProvider = new GoogleAuthProvider();
+  const data = await signInWithPopup(auth, googleProvider);
+  return data.user.email;
+});
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {},
+  reducers: {
+    logout: (state) => {
+      state.email = "";
+    },
+    setUser: (state, payload) => {
+      state.email = payload;
+      state.isLoading = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.email = payload;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.email = "";
+        state.isError = true;
+        state.error = action.error.message;
+      })
+      .addCase(loginUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.email = payload;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.email = "";
+        state.isError = true;
+        state.error = action.error.message;
+      })
+      .addCase(googleLogin.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(googleLogin.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.email = payload;
+        state.isError = false;
+        state.error = "";
+      })
+      .addCase(googleLogin.rejected, (state, action) => {
+        state.isLoading = false;
+        state.email = "";
+        state.isError = true;
+        state.error = action.error.message;
+      })
+  },
 });
-
-
-export default authSlice.reducer
+export const { logout, setUser } = authSlice.actions;
+export default authSlice.reducer;
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+    apiKey: "AIzaSyBehF1D5I8wo2igicYDOv59KornQffoulw",
+    authDomain: "jobbox-redux-7b0cd.firebaseapp.com",
+    projectId: "jobbox-redux-7b0cd",
+    storageBucket: "jobbox-redux-7b0cd.appspot.com",
+    messagingSenderId: "563220117639",
+    appId: "1:563220117639:web:43c74de327302e3539a022"
 };
 
 // Initialize Firebase
